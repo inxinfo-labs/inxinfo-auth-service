d6f29613fed28ec53594cfdf91b44d25ca1e00f6
satishlabs <spd61185@gmail.com>
2026-01-23 09:22:03 +0530

    Updated properties file

--- src/main/java/com/satishlabs/auth/config/CorsConfig.java
+++ src/main/java/com/satishlabs/auth/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.satishlabs.auth.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,10 +10,14 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://www.inxinfo.com",
+                        "https://inxinfo.com",
+                        "https://inxinfo-user-portal-1.onrender.com"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
-
